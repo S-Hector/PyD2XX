fd9d5c8adacc61854b9d09243bcfcf4b681086cd
Hector-S <70670668+S-Hector@users.noreply.github.com>
2026-09-04 14:16:04 -0700

    Fix example 05_linux_grant_user_access.py

--- Example Code/API Python/05_linux_grant_user_access.py
+++ Example Code/API Python/05_linux_grant_user_access.py
@@ -4,7 +4,7 @@ CUSTOM_VID = int("0x1234",16)
 CUSTOM_PID = int("0xABCD",16)
 
 # User access lasts until device is unplugged/reset/power cycled.
-Status = PyD2XX.register_ftdi_sio(VID = CUSTOM_VID, PID = CUSTOM_PID)
+Status = PyD2XX.linux_grant_user_access(VID = CUSTOM_VID, PID = CUSTOM_PID)
 if(Status != PyD2XX.FT_OK):
     print(PyD2XX.FT_STATUS_STR[Status] + " | FAILED TO GRANT USER ACCESS: ABORTING")
     exit()
